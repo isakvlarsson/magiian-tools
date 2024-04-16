c0a148c8de4d7791e92ae9402223bf3f58f4dcc1
Isak Larsson <isak.valter.larsson@gmail.com>
2024-04-16 12:37:47 +0200

    Add outcome generation and write to file

--- outcome_test.pl
+++ outcome_test.pl
@@ -1,16 +1,26 @@
-:- dynamic visited_goto/2.
+:- dynamic visited_goto/2, unique_simple_outcome/3.
 
 wagon_query(K_max):-
 	G = wagon_game_short, 
 	load_game(G), 
-	create_expanded_game(G, K_max), 
-	create_outcome_graph(G, K_max), 
+	create_expanded_game(G, K_max),  
 	iterate_k_levels(G, 1, K_max).
 
 
 iterate_k_levels(G, K_max + 1, K_max).
 iterate_k_levels(G, K, K_max):-
-	unique_outcome(G, K, Outcome).
+	create_outcome_graph(G, K),
+	retractall(visited_goto(G, K, _, _)),
+	retractall(unique_simple_outcome(G, K, _)),
+	format(atom(Filename), 'outcomes/~a_K~a_outcomes.txt', [G, K]),
+	open(Filename, write,Out),
+	forall(unique_outcome(G, K, Outcome),
+		(outcome_as_locations(G, K, Outcome, Locations),
+		(unique_simple_outcome(G, _, Locations) -> (true);
+			(assertz(unique_simple_outcome(G, K, Locations)), writeln(Out, Locations)))
+		)
+	; true),
+	close(Out).
 	
 %% Is true if Outcome is a outcome (list of nodes in the outcome graph) of G at level K
 outcome(G, K, Outcome):-
@@ -46,7 +56,7 @@ unique_outcome(G, K, Outcome, Accumulator, Last):-
 	outcome_graph_node(G, K, L, N) = Last,
 	outcome_graph_goto_edge(G, K, N, Start, Id, Act, FinalS, History), % If there is a goto-edge from this node
 	outcome_graph_goto(G, K, N, Start, Back, Id),
-	(visited_goto(Start, N) -> fail; assertz(visited_goto(Start, N))),
+	(visited_goto(G, K, Start, N) -> fail; assertz(visited_goto(G, K, Start, N))),
 	Last2 = outcome_graph_goto(G, K, N, Start, Back, Id),
 	append(Accumulator, [Last2], Accumulator2),
 	Outcome = Accumulator2.
